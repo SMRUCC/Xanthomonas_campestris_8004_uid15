f93d62b176e21ec29f4128603bbe4dbca8dc39a6
xieguigang <xie.guigang@gmail.com>
2017-02-10 03:00:42 +0800

    update fasta title

--- genome/palindrome.bat
+++ genome/palindrome.bat
@@ -1,3 +1,4 @@
 #/bin/bash
 
 seqtools /Promoter.Regions.Parser.gb /gb "../genbank/CP000050.1.txt"
+seqtools /Promoter.Regions.Palindrome /in "../genbank/CP000050.1.txt" /out "./palindrome_promoter=-250bp.csv"
