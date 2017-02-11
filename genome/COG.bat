f1a268c46d95f7bbf350cdd3ca039981a55832ce
xieguigang <xie.guigang@gmail.com>
2017-02-11 23:20:39 +0800

    COG catalog profiling plot

--- genome/COG.bat
+++ genome/COG.bat
@@ -1,1 +1,2 @@
 localblast /COG2014.result /sbh "./raw/CP000050.1-protein-COG.sbh.Csv" /cog "./raw/cog2003-2014.csv" /out "./COG/Xcc8004-COGs.csv"
+eggHTS /COG.profiling.plot /in "./COG/Xcc8004-COGs.csv"
