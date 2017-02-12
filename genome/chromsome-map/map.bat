9ccd899dc42239e3e1fa64754cdac6c3fc8b6c17
xieguigang <xie.guigang@gmail.com>
2017-02-12 23:40:29 +0800

    update config data

--- genome/chromsome-map/map.bat
+++ genome/chromsome-map/map.bat
@@ -1,1 +1,3 @@
-MapPlot --Draw.ChromosomeMap.genbank /gb "../../genbank/CP000050.1.txt" /conf "./config.inf" /out "./" /COG "./COG/Xcc8004-COGs.csv"
+# Generates the config file template and you can edit it in notepad++
+# MapPlot /Config.Template /out "./config.inf"
+MapPlot --Draw.ChromosomeMap.genbank /gb "../../genbank/CP000050.1.txt" /conf "./config.inf" /out "./" /COG "../COG/Xcc8004-COGs.csv"
