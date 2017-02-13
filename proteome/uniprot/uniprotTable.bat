9f1c101e6658f75be201e6101ace026461dd461c
xieguigang <xie.guigang@gmail.com>
2017-02-13 21:11:51 +0800

    go profiling plot

--- proteome/uniprot/uniprotTable.bat
+++ proteome/uniprot/uniprotTable.bat
@@ -1,1 +1,2 @@
-eggHTS /protein.annotations /uniprot "./uniprot.xml"
+# eggHTS /protein.annotations /uniprot "./uniprot.xml"
+eggHTS /proteins.Go.plot /in ./proteins-uniprot-annotations.csv /top 20 /size 2000,4000
