281c07f1c5bf4be15e6a78c987cc0fc190caf92d
xieguigang <xie.guigang@gmail.com>
2017-02-25 00:56:25 +0800

    mirror palindrome clustering result

---
+++ genome/mirror-palindrome/kmeans.bat
@@ -0,0 +1,1 @@
+PhenoTree /locis.clustering /in "./CP000050.1_min=4,max=20,upstream=-250bp.palindrome-mirror.fasta" /cut 1 /first.ID /method NeedlemanWunsch /colors clusters /clusters 25 /out ./kmeans
