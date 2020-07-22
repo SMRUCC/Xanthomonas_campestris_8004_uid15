b3691449b1c40b64ea8f05be006915ff2efb1dfa
xieguigang <xie.guigang@gcmodeller.org>
2020-07-22 23:20:17 +0800

    Update contextDb.R

--- genome/comparative_genomics/contextDb.R
+++ genome/comparative_genomics/contextDb.R
@@ -1,15 +1,24 @@
 imports ["annotation.genbank_kit", "annotation.genomics"] from "seqtoolkit";
 
-const test.gbff as string = "K:\bacterials\GCA_001931535.1_ASM193153v1_genomic.gbff.gz";
+const bacterials_repo as string = "K:\bacterials";
+const context_table as string = "K:\bacterials_bundleAll.txt";
 
-using gbff as open.gzip(test.gbff) {
-	
-	gbff 
-	:> populate.genbank 
-	:> projectAs(genome.genes) 
-	:> unlist 
-	:> as.vector(mode = "gene_info") 
-	:> write.PTT_tabular(file = "X:\test.txt")
-	;
-
+let genbank_gz as string = list.files(bacterials_repo, pattern = "*.gz");
+let read_context as function(gbff.gz) {
+	using gbff as open.gzip(gbff.gz) {
+		gbff 
+		:> populate.genbank 
+		:> projectAs(genome.genes) 
+		:> unlist(typeof = "gene_info", pipeline = TRUE) 
+		;
+	}
 }
+
+print(`we have ${length(genbank_gz)} genbank data files:`);
+print(basename(genbank_gz));
+
+genbank_gz
+:> projectAs(read_context)
+:> unlist(typeof = "gene_info", pipeline = TRUE) 
+:> write.PTT_tabular(file = context_table)
+;
