83018abdc204934145183e852ec01f073717361f
xieguigang <xie.guigang@gcmodeller.org>
2020-07-23 20:58:56 +0800

    script for re-dump data files

--- genome/comparative_genomics/contextDb_bundleAll.R
+++ genome/comparative_genomics/contextDb_bundleAll.R
@@ -6,23 +6,20 @@ imports ["annotation.genbank_kit", "annotation.genomics"] from "seqtoolkit";
 
 # setting data source and table output
 const bacterials_repo as string = "K:\bacterials";
+const context_table as string   = "K:\bacterials_bundleAll.txt";
 
 let genbank_gz as string = list.files(bacterials_repo, pattern = "*.gz");
-let redump_context as function(gbff.gz) {
+let read_context as function(gbff.gz) {
 
 	# read a single gbff gzip compressed file
 	# and then populate all of the genomics
 	# context data objects from the given genbank
 	# data
 	using gbff as open.gzip(gbff.gz) {
-		let genbank = gbff 
+		gbff 
 		:> populate.genbank 
-		:> as.vector
-		;
-	
-		genbank[!is.plasmid(genbank)]
-		:> as.PTT
-		:> write.PTT_tabular(file = `${bacterials_repo}/${basename(gbff.gz)}.PTT`)
+		:> projectAs(genome.genes) 
+		:> unlist(typeof = "gene_info", pipeline = FALSE) 
 		;
 	}
 }
@@ -31,4 +28,8 @@ print(`we have ${length(genbank_gz)} genbank data files:`);
 print(basename(genbank_gz));
 
 # run pipeline and then save the data table
-genbank_gz :> sapply(redump_context);
+genbank_gz
+:> projectAs(read_context)
+:> unlist(typeof = "gene_info", pipeline = TRUE) 
+:> write.PTT_tabular(file = context_table)
+;
