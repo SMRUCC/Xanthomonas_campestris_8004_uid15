608cd6731a6ea6f9ef36744c01c4de1bc401d1db
xieguigang <xie.guigang@gcmodeller.org>
2020-07-26 00:13:48 +0800

    Update circos.R

--- genome/comparative_genomics/circos.R
+++ genome/comparative_genomics/circos.R
@@ -1,1 +1,10 @@
-imports "circos" from "circoskit";
+imports ["circos", "track.plots"] from "circoskit";
+imports "annotation.genbank_kit" from "seqtoolkit";
+
+const genomics = circos();
+const genbank = read.genbank("../../genbank/CP000050.1.txt");
+
+genomics
+:> backbone(source = origin.fasta(genbank), loophole = 1024)
+:> save(directory = "../circos")
+;
