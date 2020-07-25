466f51b2c5d93ad68f4d3bafc389bbaf6f591fca
xieguigang <xie.guigang@gcmodeller.org>
2020-07-26 01:08:51 +0800

    config basic model

--- genome/comparative_genomics/circos.R
+++ genome/comparative_genomics/circos.R
@@ -1,10 +1,11 @@
 imports ["circos", "track.plots"] from "circoskit";
 imports "annotation.genbank_kit" from "seqtoolkit";
 
-const genomics = circos();
+const genomics = circos(chromosomes_units = 50000);
 const genbank = read.genbank("../../genbank/CP000050.1.txt");
 
 genomics
 :> backbone(source = origin.fasta(genbank), loophole = 1024)
+:> ideogram(radius = "0.65r")
 :> save(directory = "../circos")
 ;
