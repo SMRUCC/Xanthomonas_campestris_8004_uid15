2827576b9597f0aaa032edb1d5658ebb67ad1d79
xieguigang <xie.guigang@gcmodeller.org>
2020-07-26 15:25:24 +0800

    add gene names text track

--- genome/comparative_genomics/circos.R
+++ genome/comparative_genomics/circos.R
@@ -2,6 +2,7 @@ imports ["circos", "track.plots", "model"] from "circoskit";
 imports "annotation.genbank_kit" from "seqtoolkit";
 imports "annotation.genomics_context" from "seqtoolkit";
 imports "annotation.genomics" from "seqtoolkit";
+imports "annotation.terms" from "seqtoolkit";
 
 setwd(!script$dir);
 
@@ -13,12 +14,28 @@ let [forwards, reverses] = genbank
 :> [strand.filter("+"), strand.filter("-")]
 ;
 
+# create geneName text labels for circos track plot
+let geneNames = genbank 
+:> genome.genes
+:> sapply(as.object) 
+:> lapply(gene => list(geneName = geneNames(gene$Product), gene = gene), names = gene -> gene$Synonym)
+:> which(gene => !is.empty(gene$geneName))
+:> sapply(function(obj) {
+	let loci  = as.object(obj$gene$Location);
+	let left  = loci$Start;
+	let right = loci$Ends;
+	
+	text(start = left, end = right, text = obj$geneName);
+})
+;
+
 print(`there are ${length(forwards)} genes in forward strand and ${length(reverses)} genes in reverse strand.`);
 
 genomics
 :> backbone(source = origin.fasta(genbank), loophole = 1024)
 :> ideogram(radius = "0.65r")
 :> main
+:> add(track.text(geneName))
 :> add(forwards :> highlight.genemarks(colors = "brown") :> track.highlight)
 :> add(reverses :> highlight.genemarks(colors = "blue") :> track.highlight)
 :> save(directory = "../circos")
