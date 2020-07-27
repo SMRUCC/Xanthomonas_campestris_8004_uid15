98934a93fee6c77b9138976446dba3e848f35f3d
xieguigang <xie.guigang@gcmodeller.org>
2020-07-27 19:39:22 +0800

    Update assembly_genbank.R

--- Rscript/assembly_genbank.R
+++ Rscript/assembly_genbank.R
@@ -9,11 +9,23 @@ const template as string = "../genbank/CP000050.1.txt";
 const COGsbh as string   = "../genome/COG/Xcc8004.myva.myva_COG.csv";
 
 let assembly = read.genbank(template);
+let allFeatures = assembly :> enumerateFeatures;
 
 # add COG annotation for each features
-let COG = COGsbh :> read.MyvaCOG :> assign.COG();
+let COG = COGsbh 
+:> read.MyvaCOG 
+:> assign.COG() 
+:> sapply(as.object) 
+:> lapply(a => a, names = a -> a$query_name)
+;
 
-print(head(as.data.frame(COG)));
+str(COG);
+
+head(COG) 
+:> unlist 
+:> as.data.frame 
+:> print
+;
 
 assembly
 :> write.genbank(file = "../genbank/CP000050.1.updated.gbff")
