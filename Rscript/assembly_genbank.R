2107e74e8111a68ef7d3ac21cf241f8241a18d2e
xieguigang <xie.guigang@gcmodeller.org>
2020-07-27 18:53:13 +0800

    removes the large data files

--- Rscript/assembly_genbank.R
+++ Rscript/assembly_genbank.R
@@ -1,9 +1,16 @@
 imports "annotation.genbank_kit" from "seqtoolkit";
+imports "annotation.terms" from "seqtoolkit";
 
 # Rscript for update genbank assembly file
 
 setwd(!script$dir);
 
 const template as string = "../genbank/CP000050.1.txt";
+const COGsbh as string   = "../genome/COG/Xcc8004.myva.myva_COG.csv";
 
 let assembly = read.genbank(template);
+
+# add COG annotation for each features
+let COG = COGsbh :> read.MyvaCOG :> assign.COG();
+
+print(COG);
